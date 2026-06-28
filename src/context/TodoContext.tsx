4c8e8dc05e927c69ea29e5f1ef4e42a19860c20b
akilaisuru <akilaisurukahingala@gmail.com>
2026-06-28 12:26:35 +0530

    first commit

---
+++ src/context/TodoContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const TodoContext = createContext<any>(null);
+
+export const TodoProvider = ({ children }: any) => {
+  const [todos, setTodo] = useState([]);
+
+  const fetchTodos = async () => {
+    const res = await axios.get("http://localhost:5000/api/todo");
+    setTodo(res.data);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <TodoContext.Provider value={{ todos, fetchTodos }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
